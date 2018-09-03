42ab0c35ae9c32a4d2846514c64f6936b151f8bf
JarvanMo <jarvan.mo@gmail.com>
2018-09-03 17:40:00 +0800

    Initial Commit

---
+++ OpenAliPaySDK/OpenAliPaySDK.h
@@ -0,0 +1,19 @@
+//
+//  OpenAliPaySDK.h
+//  OpenAliPaySDK
+//
+//  Created by mo on 2018/9/3.
+//  Copyright © 2018年 mo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenAliPaySDK.
+FOUNDATION_EXPORT double OpenAliPaySDKVersionNumber;
+
+//! Project version string for OpenAliPaySDK.
+FOUNDATION_EXPORT const unsigned char OpenAliPaySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenAliPaySDK/PublicHeader.h>
+
+
